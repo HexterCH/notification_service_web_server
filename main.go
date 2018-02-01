8ac2981d9318f688d0ccd105f7d54022d180a3df
EthanChou <ethan.chou@honestbee.com>
2018-02-01 21:37:42 +0800

    create and comment out get

--- main.go
+++ main.go
@@ -32,5 +32,22 @@ func main() {
     }
 	})
 
-	router.Run()
+	// router.GET("/notification", func(c *gin.Context) {
+		
+  //   message := c.Query("message")
+  //   if message != "" {
+  //     value := fmt.Sprintf("{\"class\":\"notifier\",\"args\":[\"%s\"]}", message)
+  //     redisPort.Do("RPUSH", "resque:queue:slack",  value)
+	// 	  c.JSON(200, gin.H{
+	// 		  "message": 200,
+	// 	  })
+  //   } else {
+	// 	  c.JSON(403, gin.H{
+	// 		  "message": "Please send message",
+	// 	  })
+  //   }
+	// })
+
+	router.Run(":8000")
+	
 }
