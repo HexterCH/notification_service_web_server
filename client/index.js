6fc124954b7b891c0acf4915ae2cc31fa98cd2ad
EthanChou <ethan.chou@honestbee.com>
2018-02-01 22:53:15 +0800

    integrate push api

--- client/index.js
+++ client/index.js
@@ -18,5 +18,30 @@ const toggleAuth = () => {
             // alert(data);
         }
     });
-   
+}
+
+const sendPush = () => {
+   const pushText = document.getElementById(
+        'push'
+       ).value
+    $.ajax({
+        url: 'http://localhost:8088' + '/api/push',
+        type: 'POST',
+        data: JSON.stringify({
+            "notifications": [{
+                "tokens": ["2b4c2ee343dc02147cb560c8252149dcf59c1419f896a1507bd07a11cc4c8882", "df34e4841150e469f1d0349f37bbf2d7ab83fbbd247117d977f4c519014339ed"],
+                "platform": 1,
+                "message": pushText
+            }]
+        }),
+        contentType: 'application/json',
+        async: false,
+        success: function(data) {
+           alert('好啦')
+        },
+        error: function(error) {
+          console.log(error)
+          alert('錯啦')
+        }
+      });
 }
